1075d6bae541b3ac0a96fd05f6f2331bd0dedace
Jordan <a01252477@itesm.mx>
2022-10-02 20:10:50 -0700

    Stack

--- ADT_stack.h
+++ ADT_stack.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <fstream>
 #include "ADT_LinkedList.h"
 
 using namespace std;
@@ -10,11 +9,14 @@ class Stack{
     private:
         LinkedList<T> datos;
     public:
-        Stack();
-        void push(T);
-        T top();
-        T pop();
-        bool is_empty();
+        void push(T); //Inserta un dato siguiendo las reglas de un Stack
+        T top(); //Dato hasta arriba del stack
+        T pop(); //Elimina dato hasta arriba del stack
+        bool is_empty(); //True si esta vacio
+        void show(); //Muestra el stack
+        void fill(string); //Llena el stack con datos
+        bool buscar(T); //Busca un valor en el stack
+        bool replace(int, T); //Reemplaza dato en cierta posicion
 };
 
 template <class T>
@@ -38,9 +40,38 @@ T Stack<T>::top(){
 
 template <class T>
 bool Stack<T>::is_empty(){
-    if(datos.get(0) == nullptr){
-        return true;
+    return datos.is_empty();
+}
+
+template <class T>
+void Stack<T>::show(){
+    datos.show();
+}
+
+template <class T>
+void Stack<T>::fill(string file){
+    int n;
+    T dato;
+    ifstream miArchivo(file);
+
+    if(miArchivo.is_open()){
+        miArchivo >> n;
+        for(int i = 0; i < n; i++){
+            miArchivo >> dato;
+            push(dato);
+        }
+        miArchivo.close();
     } else {
-        return false;
-    }
+        cout << "No se puede abrir el archivo" << endl;
+    } 
+}
+
+template <class T>
+bool Stack<T>::buscar(T dato){
+    return datos.buscar(dato);
+}
+
+template <class T>
+bool Stack<T>::replace(int pos, T dato){
+    return datos.replace(pos, dato);
 }
