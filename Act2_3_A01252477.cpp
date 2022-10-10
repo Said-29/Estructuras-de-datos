290b00510b09e3a5bdc66bd35bdd2c83e3958002
Jordan <a01252477@itesm.mx>
2022-10-10 15:39:36 -0700

    Act 2.3

--- Act2_3_A01252477.cpp
+++ Act2_3_A01252477.cpp
@@ -1,11 +1,14 @@
+//Librerias
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 #include "ADT_LinkedList.h"
 
 using namespace std;
 
+//Intercambiar la posicion de dos pointers
 void intercambiar_p(vector<string> *a, vector<string> *b){
     vector<string> temp;
     temp = *a;
@@ -13,6 +16,8 @@ void intercambiar_p(vector<string> *a, vector<string> *b){
     *b = temp;
 }
 
+//O(n^2)
+//Ordenamiento por el metodo de burbuja bandera
 void bubble_sort(vector<vector<string>> &barcos){
     bool interruptor = true;
 
@@ -30,6 +35,7 @@ void bubble_sort(vector<vector<string>> &barcos){
     }
 }
 
+//Leer los datos de un archivo, e insertarlos en un vector
 void leer(string archivo, vector<vector<string>> &barcos){
     string strInput, fecha, hora, puerto, UBI;
     stringstream ss;
@@ -72,8 +78,8 @@ void leer(string archivo, vector<vector<string>> &barcos){
     File.close();
 }
 
+//Ordenamiento por fecha utilizando selection sort
 void sort_by_date(vector<vector<string>> &aux_barcos) {
-    // Usando selection sort
     int menor;
     for(int i = 0; i <= aux_barcos.size(); i++) {
         menor = i; // Asignamos la posicion actual como la menor
@@ -98,12 +104,14 @@ void sort_by_date(vector<vector<string>> &aux_barcos) {
     }
 }
 
+//Muestra los datos de los barcos
 void show(vector<vector<string>> &barcos){
     for(int i = 0; i < barcos.size(); i++){
         cout << "Clave: " << barcos[i][0] << " || Fecha: " << barcos[i][1] << " || Puerto: " << barcos[i][3] << endl;
     }
 }
 
+//Busca barcos por clave y regresa una lista con ellos
 LinkedList<vector<string>> search_by_key(LinkedList<vector<string>> &barcos, string clave){
     LinkedList<vector<string>> aux_barcos;
     bool found = false;
@@ -124,35 +132,52 @@ LinkedList<vector<string>> search_by_key(LinkedList<vector<string>> &barcos, str
     return aux_barcos;
 }
 
+//Muestra la cantidad de barcos con UBI x, por mes
 void show_by_month(LinkedList<vector<string>> &barcos_M, LinkedList<vector<string>> &barcos_R){
-    string clave;
+    string clave; // variable para la UBI
+    string mesesNum[] = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}; // numero de cada mes
+    string meses[] = {"JAN", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", "NOV", "DIC"}; // texto de cada mes
+    int year = 20;
 
-    cout << "Clave: " << endl;
-    cin >> clave;
+    cout << "Clave: ";
+    cin >> clave; // Pide la clave a buscar
 
+    // Busca todos los barcos que coincidan con esa clave por cada entrada (M y R)
     LinkedList<vector<string>> barcos_clave_M = search_by_key(barcos_M, clave);
     LinkedList<vector<string>> barcos_clave_R = search_by_key(barcos_R, clave);
 
-    int total_barcos = barcos_clave_M.size() + barcos_clave_R.size();
-    int count_M, count_R = 0;
+    int total_barcos = barcos_clave_M.size() + barcos_clave_R.size(); // suma el total de barcos de cada entrada
+    int count_M = 0, count_R = 0; // se inicializa un contador de los barcos de cada entrada por mes
 
-    for(int i = 0; i < barcos_clave_M.size(); i++){
-        string fecha = (barcos_clave_M.get(i)-> info)[1];
-        string fecha_new = fecha.substr(3,2);
-        if (fecha_new == "01"){
-            count_M++;
-        }       
+    cout << setw(8) << "Mes" << setw(8) << "Year" << setw(8) << "cant-M" << setw(8) << "cant-R" << endl;
+
+    // Se repite por cada mes del año 
+    for (int k = 0; k < 12; k++)
+    {
+        count_M = 0;
+        count_R = 0;
+        
+        // busca cada barco por mes y lo va sumando a su contador (entrada M)
+        for(int i = 0; i < barcos_clave_M.size(); i++){
+            string fecha = (barcos_clave_M.get(i)-> info)[1];
+            string fecha_new = fecha.substr(3,2);
+            if (fecha_new == mesesNum[k]){
+                count_M++;
+            }       
+        }
+        
+        // busca cada barco por mes y lo va sumando a su contador (entrada R)
+        for(int j = 0; j < barcos_clave_R.size(); j++){
+            string fecha = (barcos_clave_R.get(j)-> info)[1];
+            string fecha_new = fecha.substr(3,2);
+            if (fecha_new == mesesNum[k]){
+                count_R++;
+            }       
+        }
+
+        // Imprime los datos del mes, anio, y cantidad de barcos por cada entrada ese mes
+        cout << setw(8) << meses[k] << setw(8) << year << setw(8) << count_M << setw(8) << count_R << endl;
     }
-
-    for(int i = 0; i < barcos_clave_R.size(); i++){
-        string fecha = (barcos_clave_R.get(i)-> info)[1];
-        string fecha_new = fecha.substr(3,2);
-        if (fecha_new == "01"){
-            count_R++;
-        }       
-    }
-
-    cout << "JAN " << total_barcos << " " << count_M << " " << count_R << endl; 
 }
 
 int main(){
@@ -167,6 +192,7 @@ int main(){
     bubble_sort(barcos);
     sort_by_date(barcos);
 
+    //Ordena los barcos por puerto en dos listas ligadas
     for(int i = 0; i < barcos.size(); i++){
         vector<string> aux;
         aux = barcos[i];
